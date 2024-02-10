53349427eeca3a732a36ef0f8413c201169e1f9f
Anastasiya McElnikova <sav20043+McElnikova@yandex.ru>
2024-02-10 15:59:20 +0300

    Сделана страница main на классах

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import "./index.scss";
 import { user } from "./js/status.js";//Автозалогинивание. Проверка пользователя на сервере
-import { MainPage } from "./js/pages/main/index.js";
+import { MainPage } from "./js/pages/main/MainPage.js";
 import { LoginPage } from "./js/pages/login/index.js";
 import { RegistrationPage } from "./js/pages/registration/index.js";
 import router from "./js/spa-router/index.js";
 
 
-
+const mainPage=new MainPage;
 
 const routes=user?[
-                    { path: "/", view: MainPage }, //Поменяется на MainPage когда отладится
-                    { path: "/login", view: LoginPage},
-                    { path: "/registration", view: RegistrationPage}
+                    { path: "/", view: mainPage.createPage }, 
+                    //{ path: "/login", view: LoginPage},
+                    //{ path: "/registration", view: RegistrationPage}
                   ]:
                   [
                     { path: "/", view: LoginPage }, //Поменяется на MainPage когда отладится
-                    { path: "/login", view: LoginPage},
-                    { path: "/registration", view: RegistrationPage}
+                   //{ path: "/login", view: LoginPage},
+                   // { path: "/registration", view: RegistrationPage}
                   ];
 
 
